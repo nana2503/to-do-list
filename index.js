8826f3832833ac99df393c83737cab54bd875e69
ngcna2k42k2 <87693106+nana2503@users.noreply.github.com>
2022-08-03 10:41:07 +0700

    update still bug local

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const addBtn = document.querySelector(".btn-outline-secondary")
 const taskText = document.querySelector(".text")
 const currentTask = document.querySelector("#tasks-list")
 const historyTask = document.querySelector("#history-list")
+
 addBtn.addEventListener('click', () => {
-    if (taskText.value !== "") {
-        currentTask.innerHTML += `<li style="margin-bottom: 5px"> ${taskText.value} <button type="button" class="btn btn-danger" style="margin-left: 10%; padding: 0 10px;">X</button></li>`
+   let setLocal = localStorage.setItem("todo",taskText.value) 
+    let getLocal = localStorage.getItem("todo")
+    console.log(setLocal);
+    if (taskText !== "") {
+        currentTask.innerHTML += `<li style="margin-bottom: 5px"> ${getLocal} 
+                                    <button type="button" class="btn btn-danger" style="margin-left: 10%; padding: 0 10px;">X</button>
+                                    </li>`
         taskText.value = ""
     } else {
         return "";
@@ -13,7 +19,6 @@ addBtn.addEventListener('click', () => {
     delBtn.forEach((clk) => {
         clk.addEventListener("click", () => {
             clk.parentElement.remove();
-            historyTask.innerHTML += `<li> ${clk.parentElement.innerHTML}</li>`
         })
     })
 })
